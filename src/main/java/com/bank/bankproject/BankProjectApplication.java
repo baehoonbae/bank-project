ff7dadef2e4d54f473774934ae64c6935ba4b9a7
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-02 17:22:10 +0900

    fix: 스프링 시큐리티 초기 화면 제거

--- src/main/java/com/bank/bankproject/BankProjectApplication.java
+++ src/main/java/com/bank/bankproject/BankProjectApplication.java
@@ -2,8 +2,9 @@ package com.bank.bankproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BankProjectApplication {
 
 	public static void main(String[] args) {
