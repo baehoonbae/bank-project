a5dd46bd20bfcf733e728531fca2207ca4f930e9
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-08 17:56:17 +0900

    refactor: 일부 클래스 네임 수정

--- frontend/create-react-app/src/components/HomeMenuButtons.js
+++ frontend/create-react-app/src/components/HomeMenuButtons.js
@@ -2,7 +2,7 @@ function MyButton({ title, description, backgroundColor, onClick }) {
     return (
         <button className="button" style={{ backgroundColor, marginRight: '40px' }} onClick={onClick}>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                <div style={{ marginBottom: '35px' }}>{title}</div>
+                <div className="title" style={{ marginBottom: '35px' }}>{title}</div>
                 <div style={{ fontWeight: '100', fontSize: '20.5px', wordWrap: 'break-word', textAlign: 'left' }}>{description}</div>
             </div>
         </button>
