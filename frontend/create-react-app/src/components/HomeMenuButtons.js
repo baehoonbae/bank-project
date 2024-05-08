38533235565a5f6145fbe86d6b1df7d87e2ef932
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-08 17:55:16 +0900

    refactor: button 클래스 네임 수정

--- frontend/create-react-app/src/components/HomeMenuButtons.js
+++ frontend/create-react-app/src/components/HomeMenuButtons.js
@@ -1,6 +1,6 @@
 function MyButton({ title, description, backgroundColor, onClick }) {
     return (
-        <button className="buttonStyle" style={{ backgroundColor, marginRight: '40px' }} onClick={onClick}>
+        <button className="button" style={{ backgroundColor, marginRight: '40px' }} onClick={onClick}>
             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
                 <div style={{ marginBottom: '35px' }}>{title}</div>
                 <div style={{ fontWeight: '100', fontSize: '20.5px', wordWrap: 'break-word', textAlign: 'left' }}>{description}</div>
