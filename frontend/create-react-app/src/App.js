62043353921ff5f6ef9077744dc9a52fb177e94b
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-08 18:00:10 +0900

    feat: 각 버튼 페이지 이동 기능 추가

--- frontend/create-react-app/src/App.js
+++ frontend/create-react-app/src/App.js
@@ -1,9 +1,10 @@
 import logo from './assets/logo.svg';
 import './components/app/App.css';
-import MyButton from './components/HomeMenuButtons.js';
-import Header from './components/Header.js';
 import Login from './views/Login';
 import Home from './views/Home';
+import MyPage from './views/MyPage';
+import Send from './views/Send';
+import CreateAccount from './views/CreateAccount';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,9 +13,11 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
-          <Route/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/send" element={<Send />} />
+          <Route path="/create" element={<CreateAccount />} />
         </Routes>
       </Router>
     </div>
