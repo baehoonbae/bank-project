de3ae1f23c76a755c4622267f8806abbc28311d7
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-08 17:40:49 +0900

    feat: 로그인 링크 클릭시 액션 추가

--- frontend/create-react-app/src/components/Header.js
+++ frontend/create-react-app/src/components/Header.js
@@ -1,9 +1,13 @@
-function Header({ handleLogin }) {
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+function Header() {
     return (
         <>
             <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '1300px', height: '75px' }}>
                 <nav style={{ color: 'teal', fontWeight: 'bold', fontSize: '25.5px' }}>HBbank</nav>
-                <nav><a style={{ fontSize: '16.5px' }} onClick={handleLogin}>로그인</a></nav>
+                <nav>
+                    <Link to={"/login"}>로그인</Link>
+                </nav>
             </header>
         </>
     );
