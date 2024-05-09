35fd4ee9008a3591de0563a8061d3ab0a624144e
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-09 18:35:42 +0900

    feat: Footer 컴포넌트 추가

---
+++ frontend/create-react-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+function Footer() {
+    return (
+        <>
+            <div style={{ borderTop: '2px solid #F0F0F0', width: '100%', margin: '0', marginTop: '100px' }}></div>
+        </>
+    );
+}
+
+export default Footer;
