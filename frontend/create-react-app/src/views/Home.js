93593ee89fb4d017e74ac76d9b7c96a77fa389ba
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-09 18:37:18 +0900

    feat: Home 컴포넌트의 각 버튼 클릭 시 navigation 추가

--- frontend/create-react-app/src/views/Home.js
+++ frontend/create-react-app/src/views/Home.js
@@ -1,26 +1,31 @@
 import MyButton from '../components/HomeMenuButtons.js';
 import Header from '../components/Header.js';
-
+import Footer from '../components/Footer.js';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-    
+    const navigate = useNavigate();
+
     const buttons = [
         {
-          title: "조회",
-          description: "내 계좌 목록과 잔고 및 이체 내역을 조회합니다.",
-          backgroundColor: "teal",
+            title: "조회",
+            description: "내 계좌 목록과 잔고 및 이체 내역을 조회합니다.",
+            backgroundColor: "teal",
+            onClick: () => navigate('/mypage'),
         },
         {
-          title: "이체",
-          description: "다른 계좌로 송금합니다.",
-          backgroundColor: "rgb(64, 128, 128)",
+            title: "이체",
+            description: "다른 계좌로 송금합니다.",
+            backgroundColor: "rgb(64, 128, 128)",
+            onClick: () => navigate('/send'),
         },
         {
-          title: "개설",
-          description: "새로운 계좌를 개설합니다.",
-          backgroundColor: "gray",
+            title: "금융상품",
+            description: "신규 입출금 상품에 가입합니다.",
+            backgroundColor: "gray",
+            onClick: () => navigate('/create'),
         },
-      ];
+    ];
 
     return (
         <>
@@ -37,7 +42,7 @@ function Home() {
                     />
                 ))}
             </div>
-            <div style={{ borderTop: '2px solid #F0F0F0', width: '100%', margin: '0', marginTop: '100px' }}></div>
+                <Footer/>
         </>
     );
 }
