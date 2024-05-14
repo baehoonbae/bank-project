8d462aac3482a9c6c92dcf9584de846f752293e5
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-14 17:52:15 +0900

    feat: 선택된 메뉴와 선택된 메뉴 name에 대한 스타일 추가

--- frontend/create-react-app/src/styles/Styles.js
+++ frontend/create-react-app/src/styles/Styles.js
@@ -14,7 +14,28 @@ export function Styles() {
         cursor: 'pointer',
     };
 
-    return({ inputStyle, buttonStyle } );
+
+    const menuStyle = {
+        cursor: 'pointer',
+        fontSize: '18px',
+        margin: 0,
+        padding: '10px 0',
+        backgroundColor: '#e0e0e0',
+        maxWidth: '264px',
+    };
+
+    const selectedMenuStyle = {
+        ...menuStyle,
+        backgroundColor: '#b3b3b3', 
+    };
+
+    const selectedMenuNameStyle = {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: '25px',
+    }
+
+    return ({ inputStyle, buttonStyle, menuStyle, selectedMenuStyle, selectedMenuNameStyle });
 }
 
 export default Styles;
