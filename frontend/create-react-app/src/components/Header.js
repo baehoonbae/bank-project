a42e15e47ccb8423299e55790f1520796fc34138
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-20 17:57:22 +0900

    feat: 로그아웃 기능 추가

--- frontend/create-react-app/src/components/Header.js
+++ frontend/create-react-app/src/components/Header.js
@@ -1,6 +1,14 @@
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const navigate = useNavigate();
+
+    const handleLogOut = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    }
+
     return (
         <>
             <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '1100px', height: '75px' }}>
@@ -8,9 +16,13 @@ function Header() {
                     <Link className="link" to={"/"} style={{ textDecoration: 'none' }}>HBbank</Link>
                 </nav>
                 <nav>
-                    <Link className="link" to={"/user/login"}>로그인</Link>
-                    <span style={{ borderLeft: '1px solid #e0e0e0', margin: '0 10px' }}></span>
-                    <Link className="link" to={"/user/signup"}>회원가입</Link>
+                    <Link className="link" to={"/user/signup"} style={{ display: localStorage.getItem('token') ? 'none' : '' }}>회원가입</Link>
+                    <span style={{ borderLeft: '1px solid #e0e0e0', margin: '0 10px', display: localStorage.getItem('token') ? 'none' : '' }}></span>
+                    {localStorage.getItem('token') ? (
+                        <Link className="link" to={"/"} onClick={handleLogOut}>로그아웃</Link>
+                    ) : (
+                        <Link className="link" to={"/user/login"}>로그인</Link>
+                    )}
                 </nav>
             </header>
         </>
