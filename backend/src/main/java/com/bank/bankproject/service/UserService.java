0fd86773c22fb3495dbec380b6641f55daf5ca8a
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-20 17:56:48 +0900

    feat: JWT 토큰 발급을 위한  비밀 키 생성 메소드 추가

--- backend/src/main/java/com/bank/bankproject/service/UserService.java
+++ backend/src/main/java/com/bank/bankproject/service/UserService.java
@@ -2,14 +2,25 @@ package com.bank.bankproject.service;
 
 import com.bank.bankproject.domain.User;
 import com.bank.bankproject.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final String secretKey = generateSecretKey();
 
     @Autowired
     public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
@@ -26,7 +37,7 @@ public class UserService {
         if (user.getPassword() == null || user.getPassword().isEmpty()) {
             throw new IllegalArgumentException("* 비밀번호: 필수 정보입니다.");
         }
-        if(user.getName() == null || user.getName().isEmpty()) {
+        if (user.getName() == null || user.getName().isEmpty()) {
             throw new IllegalArgumentException("* 이름: 필수 정보입니다.");
         }
         if (user.getEmail() == null || user.getEmail().isEmpty()) {
@@ -59,5 +70,35 @@ public class UserService {
     }
 
     // 로그인
+    public String login(String username, String password) {
+        User user = (User) userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            Date now = new Date();
+            Date expireTime = new Date(now.getTime() + 1800000);
+
+            return Jwts.builder()
+                    .claim("sub", username)
+                    .claim("exp", expireTime.getTime() / 1000)
+                    .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .compact();
+        } else {
+            return null;
+        }
+    }
+
     // 로그아웃
+
+
+    // 비밀 키 생성
+    public String generateSecretKey() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Failed to generate secret key", e);
+        }
+    }
 }
