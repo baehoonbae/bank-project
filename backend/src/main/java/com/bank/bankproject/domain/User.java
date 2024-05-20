5f1c1a8c806fe0cc13cb6fe8a0276c3abf019bef
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-20 15:49:56 +0900

    feat: 이름, 이메일, 생년월일, 전화번호 필드 추가

--- backend/src/main/java/com/bank/bankproject/domain/User.java
+++ backend/src/main/java/com/bank/bankproject/domain/User.java
@@ -15,14 +15,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    private String name;
-
     @Column(name = "username", unique = true)
     private String username;
 
     @Column(name = "password")
     private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "birth_date")
+    private String birthDate;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Account> accounts;
