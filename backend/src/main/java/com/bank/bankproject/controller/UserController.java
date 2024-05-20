1a42b8f28bcb49b0b555d6cf9028cfc66f6a6be6
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-20 15:50:22 +0900

    feat: 회원가입 및 유효성 검사 기능 추가

--- backend/src/main/java/com/bank/bankproject/controller/UserController.java
+++ backend/src/main/java/com/bank/bankproject/controller/UserController.java
@@ -1,8 +1,40 @@
 package com.bank.bankproject.controller;
 
-import org.springframework.stereotype.Controller;
+import com.bank.bankproject.domain.User;
+import com.bank.bankproject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    // 회원 가입
+    @PostMapping("/user/signup")
+    public ResponseEntity<String> join(@RequestBody User user) {
+        userService.signUp(user);
+        return ResponseEntity.ok("회원가입이 성공적으로 완료되었습니다.");
+    }
+
+    // 아이디 중복 확인
+    @GetMapping("/user/check/username")
+    public ResponseEntity<Boolean> checkUsername(@RequestParam String username) {
+        boolean check = userService.checkUsername(username);
+        return check ? new ResponseEntity<>(check, HttpStatus.CONFLICT) : ResponseEntity.ok(check);
+    }
+
+    // 이메일 중복 확인
+    @GetMapping("/user/check/email")
+    public ResponseEntity<Boolean> checkEmail(@RequestParam String email) {
+        boolean check = userService.checkEmail(email);
+        return check ? new ResponseEntity<>(check, HttpStatus.CONFLICT) : ResponseEntity.ok(check);
+    }
+
+    // 로그인
+    // 로그아웃
+
 
 }
