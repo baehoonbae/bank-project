e03cc5a6f42ad3a8a91ffb6efdcca944442175ee
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-20 15:51:30 +0900

    feat: findByUsername, findByEmail 메소드 추가

--- backend/src/main/java/com/bank/bankproject/repository/UserRepository.java
+++ backend/src/main/java/com/bank/bankproject/repository/UserRepository.java
@@ -4,7 +4,12 @@ import com.bank.bankproject.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<Object> findByUsername(String username);
+
+    Optional<Object> findByEmail(String email);
 }
