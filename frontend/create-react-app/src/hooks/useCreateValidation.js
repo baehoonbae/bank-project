947ef1f6a4734311eecdab94c24ac236ad3b3c15
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-21 16:01:01 +0900

    feat: 계좌 개설 유효성 검사 훅 추가

---
+++ frontend/create-react-app/src/hooks/useCreateValidation.js
@@ -0,0 +1,49 @@
+// useFormValidation.js
+import { useState } from 'react';
+
+export default function useCreateValidation() {
+    const [formState, setFormState] = useState({
+        phoneNumber: '',
+        verificationCode: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [isEmpty, setIsEmpty] = useState({
+        phoneNumber: true,
+        verificationCode: true,
+        password: true,
+        confirmPassword: true,
+    });
+
+    const [touched, setTouched] = useState({
+        phoneNumber: false,
+        verificationCode: false,
+        password: false,
+        confirmPassword: false,
+    });
+
+    const checkEmpty = async (name, value) => {
+        setIsEmpty(prevState => ({
+            ...prevState,
+            [name]: !value.trim(),
+        }));
+    };
+
+    const handleChange = (e) => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleBlur = (e) => {
+        checkEmpty(e.target.name, e.target.value);
+        setTouched(prevState => ({
+            ...prevState,
+            [e.target.name]: true,
+        }));
+    };
+
+    return { formState, handleChange, handleBlur, isEmpty, touched, };
+}
