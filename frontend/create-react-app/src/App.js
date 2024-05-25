d236f21f033b64b9562ba0a80290c82e7a192338
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-25 16:18:21 +0900

    chore: App 컴포넌트에 인증 관리 로직 추가

--- frontend/create-react-app/src/App.js
+++ frontend/create-react-app/src/App.js
@@ -6,22 +6,25 @@ import Send from './views/Send';
 import CreateAccount from './views/CreateAccount';
 import SignUp from './views/SignUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './components/AuthProvider';
 
 function App() {
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user/login" element={<Login />} />
-          <Route path="/user/mypage" element={<MyPage />} />
-          <Route path="/account/send" element={<Send />} />
-          <Route path="/account/create" element={<CreateAccount />} />
-          <Route path="/user/signup" element={<SignUp />} />
-        </Routes>
-      </Router>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user/login" element={<Login />} />
+            <Route path="/user/mypage" element={<MyPage />} />
+            <Route path="/account/send" element={<Send />} />
+            <Route path="/account/create" element={<CreateAccount />} />
+            <Route path="/user/signup" element={<SignUp />} />
+          </Routes>
+        </Router>
+      </div>
+    </AuthProvider>
   );
 }
 
