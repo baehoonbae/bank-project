70e7e3b5470f37a16cf834520d06c6af034dd960
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-26 19:25:58 +0900

    feat: 토큰 검증을 포함한 계좌 개설 엔드 포인트 추가

--- backend/src/main/java/com/bank/bankproject/controller/AccountController.java
+++ backend/src/main/java/com/bank/bankproject/controller/AccountController.java
@@ -1,8 +1,28 @@
 package com.bank.bankproject.controller;
 
-import org.springframework.stereotype.Controller;
+import com.bank.bankproject.service.AccountService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 public class AccountController {
+    private final AccountService accountService;
 
+    // 계좌 개설
+    @PostMapping("/account/create")
+    public ResponseEntity<String> createAccount(@RequestParam(required = false) String accountType,
+                                                @RequestParam(required = false) String accountPassword,
+                                                HttpServletRequest request) {
+        // 토큰을 검증합니다.
+        String token = request.getHeader("Authorization");
+        if (token == null || token.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인이 필요합니다.");
+        }
+
+        return accountService.createAccount(token, accountType, accountPassword);
+    }
 }
