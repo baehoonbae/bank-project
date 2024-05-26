00a7eee84caba011b1b1886100563392bcd63355
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-26 19:32:02 +0900

    feat: CreateAccount 뷰의 선택한 메뉴 상태 및 폼 유효성 검사 추가

--- frontend/create-react-app/src/views/CreateAccount.js
+++ frontend/create-react-app/src/views/CreateAccount.js
@@ -1,22 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header.js';
 import Footer from '../components/Footer.js';
 import Styles from '../styles/Styles.js';
+import useCreateValidation from '../hooks/useCreateValidation.js';
 
 function CreateAccount() {
+  const [selectedMenu, setSelectedMenu] = useState({
+  });
 
+  useEffect(() => {
+    const storedMenu = localStorage.getItem('selectedMenu');
+    if (storedMenu) {
+      setSelectedMenu(JSON.parse(storedMenu));
+    }
+    localStorage.removeItem('selectedMenu');
+  }, []);
+
+  useEffect(() => {
+    console.log(selectedMenu);
+  }, [selectedMenu]);
+
+  const { inputStyle, buttonStyle, menuStyle, selectedMenuStyle, selectedMenuNameStyle } = Styles();
+  const { formState, handleChange, handleBlur, isEmpty, touched, } = useCreateValidation();
+  const navigate = useNavigate();
+  const [isMatch, setIsMatch] = useState({
+    confirmPassword: false,
+  });
   const [menus, setMenus] = useState([
     { name: "HB 올인원통장", description: "HB 올인원통장 설명" },
     { name: "HB 플러스통장", description: "HB 플러스통장 설명" },
     { name: "HB 일상생활비우대통장", description: "HB 일상생활비우대통장 설명" },
     { name: "마이저금통", description: "마이저금통 설명" },
   ]);
-  const [selectedMenu, setSelectedMenu] = useState(null);
-  const { inputStyle, buttonStyle, menuStyle, selectedMenuStyle, selectedMenuNameStyle } = Styles();
+  const fields = [
+    { name: 'phoneNumber', type: 'tel', placeholder: '휴대폰 번호', onBlur: () => handleBlur({ target: { name: 'phoneNumber', value: formState.phoneNumber } }), errorMessage: '* 휴대폰 번호: 필수 정보입니다.', },
+    { name: 'verificationCode', type: 'text', placeholder: '인증 번호', onBlur: () => handleBlur({ target: { name: 'verificationCode', value: formState.verificationCode } }), errorMessage: '* 인증 번호: 필수 정보입니다.' },
+    { name: 'password', type: 'password', placeholder: '비밀번호', onBlur: () => handleBlur({ target: { name: 'password', value: formState.password } }), errorMessage: '* 비밀번호: 필수 정보입니다.', },
+    {
+      name: 'confirmPassword', type: 'password', placeholder: '비밀번호 확인', errorMessage: '* 비밀번호 확인: 필수 정보입니다.', confirmError: '* 비밀번호 확인: 비밀번호가 일치하지 않습니다.',
+      onBlur: () => {
+        const isPasswordMatch = formState.password === formState.confirmPassword;
+        const isEmpty = !formState.confirmPassword;
+        if (isPasswordMatch && !isEmpty) {
+          setIsMatch(prevState => ({
+            ...prevState,
+            confirmPassword: true,
+          }));
+        } else {
+          setIsMatch(prevState => ({
+            ...prevState,
+            confirmPassword: false,
+          }));
+        }
+        handleBlur({
+          target: {
+            name: 'confirmPassword',
+            value: formState.confirmPassword,
+          },
+        });
+      },
+    }
 
+  ]
   const handleClick = (menu) => {
     setSelectedMenu(menu);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const accountType = selectedMenu.name;
+    const accountPassword = formState['password'];
+    const confirmPassword = formState['confirmPassword'];
+    if (accountPassword !== confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    fetch('http://localhost:8080/account/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+      body: JSON.stringify({ accountType, accountPassword }),
+    })
+      .then(response => {
+        if (response.status === 200) {
+          navigate('/');
+        } else {
+          throw new Error('계좌개설에 실패했습니다.')
+        }
+        return response.json()
+      })
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
   return (
     <>
@@ -41,13 +123,23 @@ function CreateAccount() {
               <span style={selectedMenuNameStyle}>{selectedMenu.name}</span>
               <div style={{ borderTop: '2px solid #F0F0F0', width: '100%', marginTop: '12px' }}></div>
               <p style={{ marginBottom: '100px' }}>{selectedMenu.description}</p>
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <label><input type="tel" placeholder='휴대폰 번호' style={inputStyle}></input><br /></label>
-                <label><input type="text" placeholder='인증 번호' style={inputStyle} ></input><br /></label>
-                <label><input type="password" placeholder='사용할 비밀번호 4자리' style={inputStyle}></input><br /></label>
-                <label><input type="password" placeholder='비밀번호 확인' style={inputStyle}></input><br /></label>
+              <form onSubmit={(event) => handleSubmit(event, selectedMenu)} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                {fields.map((field) => (
+                  <label key={field.name}>
+                    <input
+                      type={field.type}
+                      name={field.name}
+                      value={formState[field.name]}
+                      placeholder={field.placeholder}
+                      style={inputStyle}
+                      onChange={handleChange}
+                      onBlur={field.onBlur}
+                    />
+                    <div style={{ display: isEmpty[field.name] && touched[field.name] ? 'block' : 'none', color: 'red', marginBottom: '-25px' }}>{field.errorMessage}</div><br />
+                  </label>
+                ))}
                 <input type="submit" value="계좌 개설" style={buttonStyle} />
-              </div>
+              </form>
             </div>
           }
         </div>
