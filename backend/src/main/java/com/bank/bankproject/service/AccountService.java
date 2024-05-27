f42e3e48270092469484198aca21ebf6710847ea
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-27 18:04:14 +0900

    feat: 계좌 번호 무작위 생성 로직 추가

--- backend/src/main/java/com/bank/bankproject/service/AccountService.java
+++ backend/src/main/java/com/bank/bankproject/service/AccountService.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class AccountService {
@@ -30,10 +31,7 @@ public class AccountService {
 
     // 계좌 개설
     public ResponseEntity<String> createAccount(String token, String accountType, String accountPassword) {
-        if (token != null && token.startsWith("Bearer ")) {
-            // 토큰에서 "Bearer "를 제거합니다.
-            token = token.substring(7);
-
+        if (token != null) {
             // 토큰에서 userId 를 추출하고 검증합니다.
             Long userId = userService.getUserIdFromToken(token);
             if (userId == null) {
@@ -49,6 +47,10 @@ public class AccountService {
                 newAccount.setAccountPassword(passwordEncoder.encode(accountPassword));
                 newAccount.setUser(user);
                 newAccount.setBalance(0.0);
+
+                String accountNumber = generateAccountNumber();
+                newAccount.setAccountNumber(accountNumber);
+
                 accountRepository.save(newAccount);
                 return ResponseEntity.ok("계좌가 생성되었습니다.");
             } else {
@@ -57,5 +59,15 @@ public class AccountService {
         } else {
             return ResponseEntity.status(401).body("로그인이 필요합니다.");
         }
+    }
+
+    // 계좌 번호 생성
+    public String generateAccountNumber() {
+        Random random = new Random();
+        int part1 = random.nextInt(900) + 100;
+        int part2 = random.nextInt(9000) + 1000;
+        int part3 = random.nextInt(9000) + 1000;
+
+        return "793" + part1 + "-" + part2 + "-" + part3;
     }
 }
