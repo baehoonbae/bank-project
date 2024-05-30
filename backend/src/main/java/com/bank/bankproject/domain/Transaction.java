44277961ddedce91c2da69b168d69f9eda8f992a
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-05-30 17:22:27 +0900

    feat: 거래내역 테이블 추가

--- backend/src/main/java/com/bank/bankproject/domain/Transaction.java
+++ backend/src/main/java/com/bank/bankproject/domain/Transaction.java
@@ -1,2 +1,38 @@
-package com.bank.bankproject.domain;public class Transaction {
+package com.bank.bankproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "transaction_date")
+    private LocalDateTime transactionDate;
+
+    @Column(name = "transaction_type")
+    private String transactionType;
+
+    @Column(name = "sender_or_receiver")
+    private String senderOrReceiver;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "amount")
+    private Double amount;
+
+    @Column(name = "balance_after_transaction")
+    private Double balanceAfterTransaction;
 }
